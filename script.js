fa4d270328482b060841822943b24592748744bb
VEGA <giantwhitedeer@gmail.com>
2025-04-11 02:27:34 -0400

    quiz progression

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-const button = document.getElementById("takeQuizButton");
-const button1 = document.getElementById("continueToQuiz");
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("takeQuizButton");
+    const button1 = document.getElementById("continueToQuiz");
+    const button2 = document.getElementById("styleButton");
+  
+    if (button) {
+      button.addEventListener("click", function() {
+        window.location.href = "quizStartPage.html";
+      });
+    }
+  
+    if (button1) {
+      button1.addEventListener("click", function() {
+        window.location.href = "quizFoodDrink.html";
+      });
+    }
 
-button.addEventListener("click", function(){
-    window.location.href = "quizStartPage.html";
-});
-
-button1.addEventListener("click", function(){
-    window.location.href = "quizPage1.html";
-});
+    if (button2) {
+        button2.addEventListener("click", function() {
+          window.location.href = "quizStylePage.html";
+        });
+      }
+  });
