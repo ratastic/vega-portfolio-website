d8f3f6c091b19eb011a20a85e470cc700c9ee8b2
VEGA <giantwhitedeer@gmail.com>
2025-04-10 20:59:00 -0400

    quiz button additions

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const button = document.getElementById("takeQuizButton");
+const button1 = document.getElementById("continueToQuiz");
 
 button.addEventListener("click", function(){
+    window.location.href = "quizStartPage.html";
+});
+
+button1.addEventListener("click", function(){
     window.location.href = "quizPage1.html";
 });
