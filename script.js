b2b8d038da253fc43a8553528f8e6393d7e6ac84
VEGA <giantwhitedeer@gmail.com>
2025-04-11 10:05:39 -0400

    additions

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const button = document.getElementById("takeQuizButton");
     const button1 = document.getElementById("continueToQuiz");
     const button2 = document.getElementById("styleButton");
+    const button3 = documer.getElementById("outcomeButton");
   
     if (button) {
       button.addEventListener("click", function() {
@@ -18,6 +19,12 @@ document.addEventListener("DOMContentLoaded", function() {
     if (button2) {
         button2.addEventListener("click", function() {
           window.location.href = "quizStylePage.html";
+        });
+      }
+
+      if (button3) {
+        button3.addEventListener("click", function() {
+          window.location.href = "quizResultPage.html";
         });
       }
   });
